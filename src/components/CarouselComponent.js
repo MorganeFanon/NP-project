064ba06c984db19f07672d43eaaaa3ed01b399dc
Morgane Fanon <91192049+MorganeFanon@users.noreply.github.com>
2022-02-26 11:14:41 +0100

    ajout carousel

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -1,61 +1,25 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import  Carousel from 'react-material-ui-carousel';
-import { Paper, Button } from "@mui/material";
-import one from '../Assets/one.jpg';
-import two from '../Assets/two.jpg';
-import three from '../Assets/three.jpg';
-import four from '../Assets/four.jpg';
+// import one from '../Assets/one.jpg';
+// import two from '../Assets/two.jpg';
+// import three from '../Assets/three.jpg';
+// import four from '../Assets/four.jpg';
 
 function CarouselComponent(props)
 {
-    var items = [
-        {  
-            photo: {one},
-            name: "Finally a social network for elderly",
-            description: "Probably the most random thing you have ever seen!"
-
-        },
-        {
-            photo: {two},
-            name: "Random Name #2",
-            description: "Hello World!"
-        },
-        {
-            photo: {three},
-            name: "Random Name #2",
-            description: "Hello World!"
-        },
-        {
-            photo: {four},
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
-    ]
+    const imagesPool = [
+        { src : '../Assets/one.jpg' },
+        { src : '../Assets/two.jpg' },
+        { src: '../Assets/three.jpg'},
+        { src: '../Assets/four.jpg'}
+    ];
 
     return (
-
             <Carousel>
-                {
-                    items.map( (item, i) => <Item key={i} item={item} /> )
-                }
+                {imagesPool.map(( imgSrc, index ) => (<img src={imgSrc.src} key={index} alt="Imgaes can't be displayed, whyyyyy !!!???" />))}
            </Carousel>
-        
-    )
-}
+    )};
 
-function Item(props)
-{
-    return (
-        <Paper>
-            <h2>{props.item.name}</h2>
-            <p>{props.item.description}</p>
-
-            <Button className="CheckButton">
-                Check it out!
-            </Button>
-        </Paper>
-    )
-};
 
 export default CarouselComponent
